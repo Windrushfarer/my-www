65a74ac4ee3eb8b4eb60e2229261613523e53f28
Windrushfarer <windrushfarer@gmail.com>
2022-01-29 17:49:49 +0100

    added svgr to webpack config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withOffline = require('next-offline')
 
-/** @type {import('next').NextConfig} */
-module.exports = withOffline({
+const offlineConfig = withOffline({
   pageExtensions: ["tsx"],
   trailingSlash: true,
   experimental: { esmExternals: true },
@@ -9,3 +8,25 @@ module.exports = withOffline({
     swDest: '../public/service-worker.js',
   }
 })
+
+const withSvgr = (nextConfig) => {
+  const { webpack: enhanceWebpack, ...rest } = nextConfig
+
+  return {
+    ...rest,
+    webpack(config, options) {
+      const result = enhanceWebpack(config, options)
+
+      result.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [{ loader: '@svgr/webpack', options: { typescript: true, svgo: false } }],
+      })
+
+      return result
+    }
+  }
+}
+
+/** @type {import('next').NextConfig} */
+module.exports = withSvgr(offlineConfig)
